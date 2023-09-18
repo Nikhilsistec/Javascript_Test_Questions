8af17ed2ba487d182ad656aebab6b866d8e051f5
Nikhil Suryawanshi <89860954+Nikhilsistec@users.noreply.github.com>
2023-09-18 10:55:27 +0530

    third_question
    Write an Arrow Function to Return a Boolean if a number is divisible by 10

---
+++ numisdivisibleby10.js
@@ -0,0 +1,9 @@
+const isDivisibleBy10 = (number) => {
+    return number % 10 === 0;
+  };
+  
+  // Example usage:
+  console.log(isDivisibleBy10(20)); // Output: true
+  console.log(isDivisibleBy10(15)); // Output: false
+  console.log(isDivisibleBy10(100)); // Output: true
+  
