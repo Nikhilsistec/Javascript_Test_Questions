7c90ca927c4e0dd1e113a5de6beaba41964451ce
Nikhil Suryawanshi <89860954+Nikhilsistec@users.noreply.github.com>
2023-09-18 11:04:07 +0530

    sixth_question
    What is string interpolation and concatenation. Write definition with example.

---
+++ interpolation_and_concatenation.js
@@ -0,0 +1,18 @@
+// String Interpolation:
+
+// String interpolation is a method of constructing strings by embedding expressions or variables inside a string literal. The values of the expressions or variables are evaluated and inserted into the string, creating a single, coherent string.
+
+//Example
+const name = 'John';
+const age = 30;
+const message = `Hello, my name is ${name} and I am ${age} years old.`;
+console.log(message); // Output: Hello, my name is John and I am 30 years old.
+// String Concatenation:
+
+// String concatenation is a method of joining multiple strings together to create a new string. This can be done using string concatenation operators or functions, depending on the programming language.
+const firstName = 'John';
+const lastName = 'Doe';
+const fullName = firstName + ' ' + lastName;
+console.log(fullName); // Output: John Doe
+
+
